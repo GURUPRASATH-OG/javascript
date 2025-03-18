7d35d2c8bc493532c094c1da93109762429bfbde
GURUPRASATH-OG <gp9071112@gmail.com>
2025-03-18 17:15:26 +0000

    windows work files

---
+++ todo.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('inBtn')
+const input = document.getElementById("user-input")
+const output = document.getElementById('output')
+// button.addEventListener('click',()=>
+//     {
+//         document.getElementById('op').innerHTML+=`<li>${input.value}</li>`
+//         input.value=''   
+//     }
+// )
+const submitbtn = document.getElementById('submit')
+const form = document.getElementById('input-form')
+const username = document.getElementById('user-name')
+const userage = document.getElementById('user-age')
+const list = document.getElementById('op')
+console.log(form)
+form.onsubmit=()=>{
+    const li = document.createElement('li')
+    submitbtn.role='ADD';
+    submitbtn.innerText='ADD';
+    list.append(li);
+    li.innerText=username.value;
+    li.innerText=userage.value;
+    console.log(username.value,userage.value)
+    username.value='';
+    userage.value='';
+}
